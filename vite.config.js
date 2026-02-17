3b21bf97925a83e16643416c6a293d33a7bd9f49
Hyewon_Jung <dnjs3023@gmail.com>
2026-02-18 01:30:18 +0900

    fix: 도메인 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed from '/Clarus23.github.io/' because this is a User Page repo
+  base: './',
 })
